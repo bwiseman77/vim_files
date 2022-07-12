a0299e96ae3885824f3318ab88c4697b4a9097e8
Brett Wiseman <bwisema3@nd.edu>
2022-07-12 01:27:09 -0400

    update setup

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
-echo "Downloading vimrc file"
-# mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
-# curl .vimrc
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# curl -Lo ~/.vim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+echo "setting up vimrc file"
+
+# create dirs
+mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
+
+# download vimrc
+curl -LO https://raw.githubusercontent.com/bwiseman77/vim_files/master/.vimrc
+
+# download vimplugin
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# downlaod colors
+curl -Lo ~/.vim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim

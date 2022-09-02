35cfaf1ffc3a93e129a22c2dea56016692cd4f3e
Brett Wiseman <72280180+bwiseman77@users.noreply.github.com>
2022-09-02 19:39:11 -0400

    Update to pathogen

--- vim_setup.sh
+++ vim_setup.sh
@@ -2,13 +2,22 @@
 echo "setting up vimrc file"
 
 # create dirs
-mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
+mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged ~/.vim/bundle
 
 # download vimrc
 curl -LO https://raw.githubusercontent.com/bwiseman77/vim_files/master/.vimrc
 
 # download vimplugin
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/ --create-dirs git clone https://github.com/tpope/vim-pathogen.git
 
 # downlaod colors
-curl -Lo ~/.vim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+curl -Lo ~/.vim/colors/ https://raw.githubusercontent.com/Mcmartelle/vim-monokai-bold/master/colors/monokai-bold.vim
+
+# get airline
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle
+
+# get syntastic
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle
+
+# get supertab
+git clone --depth=1 https://github.com/ervandew/supertab.git ~/.vim/bundle
